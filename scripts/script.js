f79f14aa460e1187ca874ad16682e4a583e94586
UaHig <golosnyyigor@gmail.com>
2023-04-19 18:25:08 +0200

    new graphic effects add one slash

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,14 @@
 function cycleBackgroundImages() {
   const images = ['images/1.png', 'images/2.png'];
   let currentIndex = 0;
-
   setInterval(() => {
     document.body.style.backgroundImage = `url(${images[currentIndex]})`;
-    document.body.style.transition = 'background-image 1s ease-in-out';
+    document.body.style.transition = 'background-image 10s ease-in-out';
     setTimeout(() => {
       document.body.style.transition = '';
-    }, 2000);
+    }, 11000);
     currentIndex = (currentIndex + 1) % images.length;
-  }, 5000);
+  }, 12000);
   
 }
 
@@ -93,7 +92,6 @@ function changeHeaderNoActive () {
   inputElement.style.setProperty('--placeholder-color', '#ffff'); // устанавливаем цвет текста placeholder
 
 }
-
 
 cycleBackgroundImages ();
 
