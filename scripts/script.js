d9133f4d1cd46851033f95c235ecb5dbaf440eeb
UaHig <golosnyyigor@gmail.com>
2023-04-26 16:52:58 +0200

    upgrade functions remove styles from js

--- scripts/script.js
+++ scripts/script.js
@@ -13,83 +13,75 @@ function cycleBackgroundImages() {
     currentIndex = (currentIndex + 1) % images.length;
   }, 12000);
 
-  
-}
-cycleBackgroundImages ();
 
-function headerActive () {
-  const navElement = document.querySelector("nav");
-  navElement.addEventListener('mouseover', changeHeaderActive);
 }
+cycleBackgroundImages();
 
-function headerNoActive () {
+const mediaQuery = window.matchMedia('(min-width: 991.98px)');
+// Проверка ширины экрана при загрузке страницы
+if (mediaQuery.matches) {
+  headerChange();
+}
+// Обнаружение изменения ширины экрана и выполнение функции
+mediaQuery.addListener(function (event) {
+  if (event.matches) {
+    headerChange();
+  }
+});
+
+function headerChange() {
   const navElement = document.querySelector("nav");
+  navElement.addEventListener('mouseover', changeheaderActive);
   navElement.addEventListener('mouseleave', changeHeaderNoActive);
 }
 
-function changeHeaderActive () {
+
+function changeheaderActive() {
   const containerDiv = document.querySelector(".container");
-  containerDiv.style.backgroundColor = 'white';
-  containerDiv.style.transition = "background-color 0.5s ease-in-out";
-  
+  containerDiv.classList.add('active');
 
   const gradientDiv = document.querySelector(".gradient");
-  gradientDiv.style.background = 'none';
-  gradientDiv.style.transition = "background 0.5s ease-in-out";
+  gradientDiv.classList.add('active');
 
   const Left = document.querySelector(".left-section");
-  Left.style.color = 'black';
-  Left.style.transition = "color 0.9s ease-in-out";
+  Left.classList.add('active');
 
   const central = document.querySelector(".nav-items");
-  central.style.color = "black";
-  central.style.transition = "color 0.7s ease-in-out";
+  central.classList.add('active');
 
   const right = document.querySelector(".right-section");
-  right.style.color = "black";
-  right.style.transition = "color 0.5s ease-in-out";
+  right.classList.add('active');
 
   const placeholder = document.querySelector(".middle-section-placeholder-text");
-  placeholder.style.backgroundColor = "#F7F7F7";
-  placeholder.style.backgroundImage = 'url("images/Fill 2.png")';
-  placeholder.style.transition = "backgroundImage 0.3s ease-in-out";
+  placeholder.classList.add('active');
 
   const basketImg = document.getElementById("basket-img");
   basketImg.src = "images/basket2.png";
-  
+
   const inputElement = document.getElementById('placeholderText'); // получаем элемент input, у которого нужно изменить цвет текста placeholder
   inputElement.style.color = 'black'; // устанавливаем цвет текста input
   inputElement.style.setProperty('--placeholder-color', 'black'); // устанавливаем цвет текста placeholder
 
-  // Для мобильных и планшетов сброс на header no Active после клика 
-  // setTimeout(changeHeaderNoActive, 2000);
 }
 
-function changeHeaderNoActive () {
+function changeHeaderNoActive() {
   const containerDiv = document.querySelector(".container")
-  containerDiv.style.backgroundColor = 'initial';
-  containerDiv.style.transition = "background-color 0.5s ease-in-out";
+  containerDiv.classList.remove('active');
 
   const gradientDiv = document.querySelector(".gradient");
-  gradientDiv.style.background = 'linear-gradient(180deg, rgba(51, 51, 51, 0.9) 14.31%, rgba(27, 27, 27, 0.469176) 58.6%, rgba(0, 0, 0, 0.0001) 100%)';
-  gradientDiv.style.transition = "background 0.3s ease-in-out";
+  gradientDiv.classList.remove('active');
 
   const Left = document.querySelector(".left-section");
-  Left.style.color = '#ffff';
-  Left.style.transition = "color 0.5s ease-in-out";
+  Left.classList.remove('active');
 
   const central = document.querySelector(".nav-items");
-  central.style.color = "#ffff";
-  central.style.transition = "color 0.7s ease-in-out";
+  central.classList.remove('active');
 
   const right = document.querySelector(".right-section");
-  right.style.color = "#ffff";
-  right.style.transition = "color 0.9s ease-in-out";
+  right.classList.remove('active');
 
   const placeholder = document.querySelector(".middle-section-placeholder-text");
-  placeholder.style.backgroundColor = "rgba(247, 247, 247, 0.24)";
-  placeholder.style.backgroundImage = 'url("images/Fill 1.png")';
-  placeholder.style.transition = "backgroundImage 0.9s ease-in-out";
+  placeholder.classList.remove('active');
 
   const basketImg = document.getElementById("basket-img");
   basketImg.src = "images/basket1.png";
@@ -100,71 +92,55 @@ function changeHeaderNoActive () {
 
 }
 
-  const burgerImage = document.getElementById('burgerImage');
-  burgerImage.addEventListener('click', openClose);
-  
-  function openClose() {
+const burgerImage = document.getElementById('burgerImage');
+burgerImage.addEventListener('click', openClose);
 
-    const mobileFooterContainer = document.querySelector('.mobile-footer-container');
-    const transparentDiv = document.querySelector('.transparent')
-    if (mobileFooterContainer.style.display === 'none' || mobileFooterContainer.style.display === 'null') {
-  mobileFooterContainer.style.display = 'block'
-  // transparentDiv.style.height = '250px'
-  animateHeight(transparentDiv, 230, 500);
+function openClose() {
 
- } else {
-  mobileFooterContainer.style.display = 'none'
-  // transparentDiv.style.height = '450px'
-  animateHeight(transparentDiv, 450, 500);
+  const mobileFooterContainer = document.querySelector('.mobile-footer-container');
+  const transparentDiv = document.querySelector('.transparent')
+  if (mobileFooterContainer.style.display === 'none' || mobileFooterContainer.style.display === 'null') {
+    mobileFooterContainer.style.display = 'block'
+    // transparentDiv.style.height = '250px'
+    animateHeight(transparentDiv, 230, 500);
+
+  } else {
+    mobileFooterContainer.style.display = 'none'
+    // transparentDiv.style.height = '450px'
+    animateHeight(transparentDiv, 450, 500);
 
 
- }
-    
-  };
+  }
 
- mobileFooterContainer.onclick = function(event) {
+};
+
+mobileFooterContainer.onclick = function (event) {
   let dropButton = event.srcElement.id;
   if (!isNaN(dropButton)) {
     openCloseItems(dropButton)
   }
 };
 
-  function openCloseItems(dropButton) {
-  const mobileFooterHeads = document.getElementById('mobileFooterHeads'+dropButton)
-  const mobileAboutUsDrops = document.getElementById('drops'+dropButton);
+function openCloseItems(dropButton) {
+  const mobileFooterHeads = document.getElementById('mobileFooterHeads' + dropButton)
+  const mobileAboutUsDrops = document.getElementById('drops' + dropButton);
   const mobileImg = document.getElementById(dropButton);
   if (mobileAboutUsDrops.style.display === 'none' || mobileAboutUsDrops.style.display === '') {
-          animateHeight(mobileAboutUsDrops, 100, 400);
-          mobileAboutUsDrops.style.display = 'block';
-          mobileAboutUsDrops.style.borderBottom = '1px solid #cecbcb';
-          mobileFooterHeads.style.borderBottom = 'none';
-          mobileImg.style.transform = 'rotate(90deg)';
-          mobileImg.style.transition = 'transform 0.4s ease-in-out';
-          
-        } else {
-          mobileAboutUsDrops.style.display = 'none';
-          animateHeight(mobileAboutUsDrops, 0, 400);
-          mobileFooterHeads.style.borderBottom = '1px solid #cecbcb';
-          mobileImg.style.transform = 'none';
-        }
-}
- 
+    animateHeight(mobileAboutUsDrops, 100, 400);
+    mobileAboutUsDrops.style.display = 'block';
+    mobileAboutUsDrops.style.borderBottom = '1px solid #cecbcb';
+    mobileFooterHeads.style.borderBottom = 'none';
+    mobileImg.style.transform = 'rotate(90deg)';
+    mobileImg.style.transition = 'transform 0.4s ease-in-out';
 
-const mediaQuery = window.matchMedia('(min-width: 991.98px)');
-
-// Проверка ширины экрана при загрузке страницы
-if (mediaQuery.matches) {
-  headerActive();
-  headerNoActive();
-}
-
-// Обнаружение изменения ширины экрана и выполнение функции
-mediaQuery.addListener(function(event) {
-  if (event.matches) {
-    headerActive();
-    headerNoActive();
+  } else {
+    mobileAboutUsDrops.style.display = 'none';
+    animateHeight(mobileAboutUsDrops, 0, 400);
+    mobileFooterHeads.style.borderBottom = '1px solid #cecbcb';
+    mobileImg.style.transform = 'none';
   }
-});
+}
+
 
 
 
